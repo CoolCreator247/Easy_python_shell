1ab99ae4377262035333af4fe265991195b12cb4
Faris Allafi <farisqaisallafi@gmail.com>
2022-06-17 23:05:13 +0000

    running the code

--- code.py
+++ code.py
@@ -1,22 +1,21 @@
-
+import time
 def intro():
     print("what is your name")
     name = input("name>")
+    time.sleep(2)
     print(f"Hi",name) 
     print("Welcome to easy shell")
     print("If you need help type help in the shell")
     #inform
-def start_shell():
-    shell = input(">")
-    #get input 
+    
+   
 def run_shell():
+    shell = input(">")
     exec(shell)
-    exec_code()
     #run code from the shell var
   
 def run_all():
     intro()
-    start_shell()
     run_shell()
 run_all()            
 
